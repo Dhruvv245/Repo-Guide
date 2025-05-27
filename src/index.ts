235a19eb1926fec182fc371b2f58fae1f8818f1c
Dhruvv245 <123222549+Dhruvv245@users.noreply.github.com>
2025-05-27 15:56:41 +0530

    tsconfig updated

--- src/index.ts
+++ src/index.ts
@@ -1,5 +1,6 @@
 #!/usr/bin/env node
 
 const user = 'Dhruv';
-console.log('Nice to meet you, ' + user);
-console.log('This is a simple Node.js script that greets the user.');
+console.log('Testing the cli');
+
+
