e3ffaba0502c2158314b9c791cd2afae905e6c9b
Dhruvv245 <123222549+Dhruvv245@users.noreply.github.com>
2025-05-28 15:11:20 +0530

    Cloning repo feature added using simple-git

--- src/index.ts
+++ src/index.ts
@@ -2,20 +2,26 @@
 
 import { Command } from 'commander';
 
-const program = new Command();
+import { cloneRepo } from './cloneRepo';
 
-program
-  .name('repo-buddy')
-  .description('CLI to help you understand any code base.')
-  .version('1.0.0');
+const program: Command = new Command();
+
+program.name('repo-buddy').description('CLI to help you understand any code base.').version('1.0.0');
 
 program
   .command('introduce')
-  .description('This command introduces you to our CLI')
-  .action(() => {
-    console.log(
-      'Hi there! This is CLI that helps you understand any code base'
-    );
+  .description('Get to know about the CLI')
+  .action((): void => {
+    console.log('Hi there! This is a CLI that helps you understand any code base');
   });
 
-program.parse();
+program
+  .command('generate')
+  .description('Generate the repo guide')
+  .argument('<repo-url>', 'GitHub repositry URL')
+  .argument('<out-dir>', 'Output directory')
+  .action(async (repoURL: string, outDir: string) => {
+    const repoPath = await cloneRepo(repoURL, outDir);
+  });
+
+program.parse(process.argv);
